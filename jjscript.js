fb2e3910ffae74c98db1cd73eb1f609815950211
Deepanshu Hardaha <deepanshuh143@gmail.com>
2017-09-16 14:16:20 +0530

    Added Delete Option

--- jjscript.js
+++ jjscript.js
@@ -3,7 +3,7 @@ $("#iform").submit(function(event){
 	var item=$("#box").val();
 	if(item)
 		{
-			$("#list").append("<li> <div class='pretty o-success plain'> <input class='checkbox' type='checkbox'/><label style='font-size:22px;'><i class='fa fa-check'></i>&nbsp&nbsp"+item+"&nbsp&nbsp</label></div><hr></li>");
+			$("#list").append("<li> <div class='pretty o-success plain'> <input class='checkbox' type='checkbox'/><label style='font-size:22px;'><i class='fa fa-check'></i>&nbsp&nbsp"+item+"&nbsp&nbsp</label></div><a class='delete'></a><hr></li>");
 		}
 		$(document).on("change",".checkbox",function(){
 			if($(this).attr("checked"))
@@ -15,5 +15,7 @@ $("#iform").submit(function(event){
 				$(this).attr("checked","checked");
 			}
 		});
-		
+		$(document).on("click",".delete",function(){
+			$(this).parent().remove();
+		});
 });
