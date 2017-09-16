26de1d8fa86bcbf0ae8393f6ac9ff9508a7b7226
Deepanshu Hardaha <deepanshuh143@gmail.com>
2017-09-16 14:03:12 +0530

    Added Mark As Done Feature

--- jjscript.js
+++ jjscript.js
@@ -3,6 +3,17 @@ $("#iform").submit(function(event){
 	var item=$("#box").val();
 	if(item)
 		{
-			$("#list").append("<li>&nbsp&nbsp"+item+"&nbsp&nbsp<hr></li>");
+			$("#list").append("<li> <div class='pretty o-success plain'> <input class='checkbox' type='checkbox'/><label style='font-size:22px;'><i class='fa fa-check'></i>&nbsp&nbsp"+item+"&nbsp&nbsp</label></div><hr></li>");
 		}
+		$(document).on("change",".checkbox",function(){
+			if($(this).attr("checked"))
+			{
+				$(this).removeAttr("checked");
+			}
+			else
+			{
+				$(this).attr("checked","checked");
+			}
+		});
+		
 });
